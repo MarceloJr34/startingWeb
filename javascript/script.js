ecbf9aadfdf527477ac028afa992672f745ae8f6
MarceloJr34 <149018381+MarceloJr34@users.noreply.github.com>
2023-10-25 22:53:41 -0700

    script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,26 @@
-function menuShow()
-{
-    let menuMobile=document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open'))
-    {
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-    }
-    else
-    {
+    } else {
         menuMobile.classList.add('open');
     }
-    
+
+    scrollToTop(600); // Tempo de rolagem em milissegundos (2 segundos)
 }
+
+function scrollToTop(duration) {
+    const scrollStep = -window.scrollY / (duration / 15);
+    
+    const scrollInterval = setInterval(function() {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 15);
+}
+
+
+
+
